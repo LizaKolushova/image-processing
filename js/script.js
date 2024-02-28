162ac891bc2bd319fdf63589c40af8cc641c6012
LizaKolushova <taeknork@gmail.com>
2024-02-28 23:03:05 +0300

    grid

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let img = new Image();
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+let colorPickerActive = false;
 
-document.getElementById('load-image').addEventListener('click', function() {
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+document.getElementById('load-image').addEventListener('click', function(e) {
+  e.preventDefault();
   const fileInput = document.getElementById('file-input');
   const urlInput = document.getElementById('url-input');
   
@@ -25,7 +44,7 @@ document.getElementById('load-image').addEventListener('click', function() {
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
             
-            document.getElementById('image-info').innerText = `Image Size: ${img.width} x ${img.height} pixels`;
+            document.getElementById('image-info').innerText = `Image Size: ${img.width} x ${img.height}`;
           }
           img.src = URL.createObjectURL(blob);
         });
@@ -41,15 +60,18 @@ document.getElementById('load-image').addEventListener('click', function() {
           canvas.height = img.height;
           ctx.drawImage(img, 0, 0);
           
-          document.getElementById('image-info').innerText = `Image Size: ${img.width} x ${img.height} pixels`;
+          document.getElementById('image-info').innerText = `Image Size: ${img.width} x ${img.height}`;
         }
         img.src = URL.createObjectURL(blob);
       });
+      
   }
+  toggleModal();
 });
 
+
 document.getElementById('reset-image').addEventListener('click', function() {
-  img.src = '';
+  loadedImage.src = '';
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   document.getElementById('file-input').value = '';
   document.getElementById('url-input').value = '';
@@ -58,20 +80,20 @@ document.getElementById('reset-image').addEventListener('click', function() {
 
 canvas.addEventListener('mousemove', function(e) {
   const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+  const x = Math.round(e.clientX - rect.left);
+  const y = Math.round(e.clientY - rect.top);
   
   document.getElementById('coordinates-info').innerText = `Coordinates: (${x}, ${y})`;
 });
 
 canvas.addEventListener('click', function(e) {
   const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+  const x = Math.round(e.clientX - rect.left);
+  const y = Math.round(e.clientY - rect.top);
 
   const imageData = ctx.getImageData(x, y, 1, 1).data;
 
-  const colorInfo = `Color: RGB(${imageData[0]}, ${imageData[1]}, ${imageData[2]})`;
+  const colorInfo = `Color: RGB (${imageData[0]}, ${imageData[1]}, ${imageData[2]})`;
   
   const colorCircle = document.createElement('div');
   colorCircle.className = 'color-circle';
@@ -83,3 +105,14 @@ canvas.addEventListener('click', function(e) {
   colorCircleContainer.innerHTML = '';
   colorCircleContainer.appendChild(colorCircle);
 });
+
+// document.getElementById('color-picker').addEventListener('click', function () {
+//   colorPickerActive = !colorPickerActive;
+//   if (colorPickerActive) {
+//       // Add logic to enable color picker (you can implement color picking functionality here)
+//       alert('Color Picker Activated!');
+//   } else {
+//       // Add logic to disable color picker
+//       alert('Color Picker Deactivated!');
+//   }
+// });
